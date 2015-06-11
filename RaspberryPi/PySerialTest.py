08e14add294e462cdadbff4cb76d7cc48a7336b4
Brent Williams <brent.robert.williams@gmail.com>
2015-06-11 15:59:00 +0000

    Added device path to serial script

--- RaspberryPi/PySerialTest.py
+++ RaspberryPi/PySerialTest.py
@@ -2,7 +2,13 @@ from time import sleep
 import serial
 
 def main():
-   ser = serial.Serial('/dev/cu.usbmodem1451', 9600) # Establish the connection on a specific port
+   # Path for raspberry pi
+   arduionoPath = '/dev/ttyACM0'
+
+   # Path for Mac
+   # arduionoPath = '/dev/cu.usbmodem1451'
+
+   ser = serial.Serial(arduionoPath, 9600) # Establish the connection on a specific port
    
    while True:
         print ser.readline() # Read the newest output from the Arduino
